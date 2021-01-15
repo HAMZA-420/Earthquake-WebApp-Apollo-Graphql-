eb813e9ebac93802cc18b9de54504688982c99d4
MUHAMMAD HAMZA KHAN <mhamzakhan.cui@gmail.com>
2021-01-15 07:48:25 +0500

    quakeReducer

---
+++ server/datasources/quake.js
@@ -0,0 +1,59 @@
+const { RESTDataSource } = require('apollo-datasource-rest');
+
+class QuakeAPI extends RESTDataSource {
+  constructor() {
+    super();
+    this.baseURL = 'https://earthquake.usgs.gov/fdsnws/event/1/';
+  }
+
+  async getAllQuakes() {
+
+    const query = 'query?format=geojson&starttime=2014-01-01&endtime=2014-01-02'
+    const response = await this.get(query);
+    return Array.isArray(response.features)
+      ? response.features.map(quake => this.quakeReducer(quake))
+      : [];
+  }
+
+  quakeReducer(quake) {
+    
+    const date = new Date(quake.properties.time)
+    const year = date.getFullYear();
+    const month = monthName(date.getMonth());
+    const day = date.getDate();
+    const hour = date.getHours();
+    const minute = date.getMinutes() <10 ? "0" + date.getMinutes() : date.getMinutes();
+    const seconds = date.getSeconds();
+    const datestring = `${month} ${day}, ${year} at ${hour}:${minute} and ${seconds} seconds`;
+    const timestamp = quake.properties.time
+
+    function monthName(index) {
+        const monthLegend = {
+            0:'Jan',
+            1:'Feb',
+            2:'March',
+            3:'April',
+            4:'May',
+            5:'June',
+            6:'July',
+            7:'Aug',
+            8:'Sept',
+            9:'Oct',
+            10:'Nov',
+            11:'Dec'
+        }
+        return monthLegend[index];
+    };
+    return {
+        mangnitude: quake.properties.mag,
+        location: quake.properties.place,
+        when: datestring,
+        time: quake.properties.time,
+        id: quake.id
+    };
+  }
+  
+  
+}
+
+module.exports = QuakeAPI;
--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
+const QuakeAPI = require('./datasources/quake');
 
-const server = new ApolloServer({ typeDefs });
+const server = new ApolloServer({ 
+  typeDefs,
+  dataSources: () => ({
+    QuakeAPI: new QuakeAPI(),
+ })
+});
 
 server.listen().then(() => {
     console.log(`
